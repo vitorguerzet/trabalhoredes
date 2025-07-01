9d69dd317c9467235988b6fbc9c39441447d8abb
vitorguerzet <48018463+vitorguerzet@users.noreply.github.com>
2025-07-01 15:15:09 -0300

    Add files via upload

---
+++ c1/prog1.py
@@ -0,0 +1,29 @@
+import socket
+import time
+import numpy as np
+import pickle
+
+HOST = '192.168.124.2'
+PORT = 6000
+k = int(input("Digite o numero de matrizes:"))
+n = int(input("Digite o tamanho da matriz quadrada: "))
+start_time = time.time()
+for i in range (k):
+
+    matriz = np.random.randint(1, 10, (n, n))
+
+#    start_time = time.time()
+
+    data = {
+        'matriz': matriz,
+        'start': start_time,
+        'k' : k
+    }
+
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp.connect((HOST, PORT))
+    tcp.send(pickle.dumps(data))
+    tcp.close()
+print('fim.')
+
+
